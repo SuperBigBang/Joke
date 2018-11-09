981ff3761c11addee2657d1e8b05f847c1c326b7
SuperBigBang <superbigbang@yandex.ru>
2018-11-09 16:55:58 +0900

    feat: add Handler (with Toast) realisation on Service

--- app/src/main/java/com/hfad/joke/service/delayedMessage/DelayedMessageService.java
+++ app/src/main/java/com/hfad/joke/service/delayedMessage/DelayedMessageService.java
@@ -2,15 +2,22 @@ package com.hfad.joke.service.delayedMessage;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.Nullable;
-
-import timber.log.Timber;
+import android.widget.Toast;
 
 public class DelayedMessageService extends IntentService {
     public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
+    }
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -27,6 +34,7 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        Timber.e("DelayedMessageService The message is: %s", text);
+        //Timber.e("DelayedMessageService The message is: %s", text);
+        handler.post(() -> Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show());
     }
 }
