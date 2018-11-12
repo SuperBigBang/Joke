7e4e37b33bbc3b0248ea2ab7e66e3645cdd03287
SuperBigBang <superbigbang@yandex.ru>
2018-11-12 17:04:01 +0900

    feat: Notification's feature realised

--- app/src/main/java/com/hfad/joke/ExtendApplication.java
+++ app/src/main/java/com/hfad/joke/ExtendApplication.java
@@ -1,6 +1,9 @@
 package com.hfad.joke;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 import timber.log.Timber;
 
@@ -30,5 +33,22 @@ public class ExtendApplication extends Application {
                 .starbuzzDatabaseHelperModule(new StarbuzzDatabaseHelperModule(this))
                 .build();
 */
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.app_name);
+            String description = "Канал с дичью";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("5", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- app/src/main/java/com/hfad/joke/service/delayedMessage/DelayedMessageService.java
+++ app/src/main/java/com/hfad/joke/service/delayedMessage/DelayedMessageService.java
@@ -1,24 +1,33 @@
 package com.hfad.joke.service.delayedMessage;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+
+import com.hfad.joke.R;
+import com.hfad.joke.screen.mainActivity.MainActivity;
+
+import timber.log.Timber;
 
 public class DelayedMessageService extends IntentService {
     public static final String EXTRA_MESSAGE = "message";
-    private Handler handler;
+    public static final int NOTIFICATION_ID = 99996;
+    //  private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
     }
 
-    @Override
+/*    @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
-        handler = new Handler();
+ //       handler = new Handler();
         return super.onStartCommand(intent, flags, startId);
-    }
+    }*/
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
@@ -34,7 +43,32 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
+
         //Timber.e("DelayedMessageService The message is: %s", text);
-        handler.post(() -> Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show());
+        //       handler.post(() -> Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show());
+        Intent intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+     /*   Notification notification=new Notification.Builder(getApplicationContext())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(text)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();*/
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "5")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(text)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
+                .setContentIntent(pendingIntent);
+        Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+        Timber.e("Канал уведомлений этого приложения");
     }
 }
